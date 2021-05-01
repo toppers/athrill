281bb263dc3dc12acd1366e8d53fd1c0489d66cd
mikoto2000 <mikoto2000@gmail.com>
2021-05-01 18:50:25 +0900

    atnrill に、 athrill_remote 用ポート番号指定機能を追加。

--- src/lib/cui/udp/cui_ops_udp.h
+++ src/lib/cui/udp/cui_ops_udp.h
@@ -1,6 +1,6 @@
 #ifndef _CUI_OPS_UDP_H_
 #define _CUI_OPS_UDP_H_
 
-extern void cui_ops_udp_init(void);
+extern void cui_ops_udp_init(uint16 server_port, uint16 client_port);
 
 #endif /* _CUI_OPS_UDP_H_ */
