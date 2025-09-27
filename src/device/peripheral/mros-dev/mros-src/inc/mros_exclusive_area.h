b9af5d174830c5452bc1383a56d98fd58b9e40eb
Takashi Mori <kanetugu2015@gmail.com>
2025-09-27 18:17:21 +0900

    macos build error is fixed

--- src/device/peripheral/mros-dev/mros-src/inc/mros_exclusive_area.h
+++ src/device/peripheral/mros-dev/mros-src/inc/mros_exclusive_area.h
@@ -1,20 +1,21 @@
-#ifndef _MROS_EXCLUSIVE_AREA_H_
-#define _MROS_EXCLUSIVE_AREA_H_
-#ifdef __cplusplus
-extern "C" {
+#ifndef _MROS_TYPES_H_
+#define _MROS_TYPES_H_
+
+/* 共通型定義など… */
+
+/* プラットフォーム別セクション属性 */
+#if defined(__APPLE__)
+  /* macOS: Mach-O は __SEGMENT,__section の形式 */
+  #define MROS_SECTION_DEF __attribute__((section("__DATA,__nc_bss"))) __attribute__((used))
+#elif defined(__linux__)
+  /* Linux: ELF はそのまま */
+  #define MROS_SECTION_DEF __attribute__((section("NC_BSS")))
+#elif defined(_WIN32) && defined(_MSC_VER)
+  /* Windows (MSVC) */
+  #define MROS_SECTION_DEF __declspec(allocate(".nc_bss"))
+#else
+  /* それ以外は属性なし */
+  #define MROS_SECTION_DEF
 #endif
 
-#include "mros_exclusive_ops.h"
-#include "mros_wait_queue.h"
-
-extern mRosExclusiveObjectType mros_exclusive_area MROS_MATTR_BSS_NOCLR;
-extern mRosWaitQueueType mros_master_wait_queue MROS_MATTR_BSS_NOCLR;
-extern mRosWaitQueueType mros_subscribe_wait_queue MROS_MATTR_BSS_NOCLR;
-
-extern void mros_exclusive_area_init(mRosTaskIdType mas_task_id, mRosTaskIdType sub_task_id);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* _MROS_EXCLUSIVE_AREA_H_ */
+#endif /* _MROS_TYPES_H_ */
